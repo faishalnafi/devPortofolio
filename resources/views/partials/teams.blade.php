58fd6772e07b1d4190c8fe55a530c744762f5af6
faishalnafi <faishalnafi50@gmail.com>
2024-03-26 20:38:25 +0700

    Tambah Anggota Tim Elian

--- resources/views/partials/teams.blade.php
+++ resources/views/partials/teams.blade.php
@@ -110,6 +110,26 @@
                 </div>
             </div>
 
+            <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
+                <div class="member">
+                    <div class="member-img">
+                        <img src="{{ asset('assets/img/team/user.png') }}" class="img-fluid" alt="">
+                        <div class="social">
+                            <a href=""><i class="bi bi-twitter"></i></a>
+                            <a href=""><i class="bi bi-facebook"></i></a>
+                            <a href=""><i class="bi bi-instagram"></i></a>
+                            <a href=""><i class="bi bi-linkedin"></i></a>
+                        </div>
+                    </div>
+                    <div class="member-info">
+                        <h4>Elian Hady</h4>
+                        <span>Developer</span>
+                        <p>Rerum voluptate non adipisci animi distinctio et deserunt amet voluptas. Quia aut aliquid
+                            doloremque ut possimus ipsum officia.</p>
+                    </div>
+                </div>
+            </div>
+
         </div>
 
     </div>
