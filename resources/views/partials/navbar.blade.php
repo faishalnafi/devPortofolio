ceb65ba94526ded833f655c5d0ee38b94eeb6d92
faishalnafi <faishalnafi50@gmail.com>
2024-11-30 16:15:15 +0700

    update: rename company

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -4,7 +4,7 @@
 
         <a href="/" class="logo d-flex align-items-center">
             <img src="{{ asset('assets/img/logo.png') }}" alt="TransLife By Nafi'">
-            <span>TransLife</span>
+            <span>MangoLab</span>
         </a>
 
         <nav id="navbar" class="navbar">
