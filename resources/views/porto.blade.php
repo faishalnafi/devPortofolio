562f5e3d262142b2501557686201dfe60041bac3
faishalnafi <faishalnafi50@gmail.com>
2024-12-07 15:28:46 +0700

    update: edit ui halaman portofolio

--- resources/views/porto.blade.php
+++ resources/views/porto.blade.php
@@ -3,7 +3,5 @@
 @section('conten')
     @include('partials.bread')
     @include('partials.port')
-    @include('partials.price')
-    @include('partials.service')
     @include('partials.footer')
 @endsection
